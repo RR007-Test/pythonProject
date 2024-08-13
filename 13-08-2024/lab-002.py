ef781ddef6232eab2b170c3d7837f8977cdbfd3f
RR007-Test <raj.hrm03@gmail.com>
2024-08-13 23:05:16 +0530

    13-aug Task

---
+++ 13-08-2024/lab-002.py
@@ -0,0 +1,10 @@
+#task2
+
+a=int(input("Enter first digit"))
+b=int(input("Enter second digit"))
+print(f"Sum of a and b is:{a+b} ")
+print(f"Sub of a and b is:{a-b}")
+print(f"Mul of a and b is:{a*b}")
+print(f"a divided by b is:{a/b}")
+print(f"max in  a and b is:{max(a,b)}")
+print(f"a to the power b is:{pow(a,b)}")
